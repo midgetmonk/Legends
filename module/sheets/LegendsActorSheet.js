a442e7f336bc6e335797a08d2704bee94e0e9061
midgetmonk <ryan.long.thomas@gmail.com>
2023-02-08 17:51:18 -0500

    update roll logic

--- module/sheets/LegendsActorSheet.js
+++ module/sheets/LegendsActorSheet.js
@@ -212,7 +212,8 @@ export default class LegendsActorSheet extends ActorSheet {
 
     const name = statName ? game.i18n.localize(`RyanTestLegends.stats.${statName}`) : null;
 
-    let conditions = this.item.system.condition;
+    let conditions = this.items;
+    conditions = filter_items(this.items, 'condition', false);
 
     console.log(conditions);
 
